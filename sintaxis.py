8e2a3ce1b1d5b0259751ed5e677287b53956d8a2
Sebastian Agudelo <sebastian.agudelo083@gmail.com>
2025-12-20 21:47:50 -0500

    feat : strings, upper case, strip and in/not in

--- sintaxis.py
+++ sintaxis.py
@@ -15,7 +15,7 @@ print(a,b,c)
 print("Mi fruta favorita es " + z)
 print(a+z)
 
-"""
+
 
  # ordenada y mutable, sus elementos pueden cambiar
 lista = [0, 1, 2, 3, 4, 5]     
@@ -76,3 +76,43 @@ print(flotante_complejo, type(flotante_complejo)) # (5.5+0j)
 import random
 
 print(random.randrange(1, 10))  # entero aleatorio entre 1 y 9
+
+
+"""
+
+father = "padre en ingles"
+mother = 'Madre en ingles'
+son = 'Father and mother "project" '
+daugther = " father's and mother's creation "
+palabra_larga = "esternocleidomastoideo"
+triple_frase = """sexo
+drogas
+y mucho alchol
+"""
+print(triple_frase)
+
+#Indica la cantidad de caracteres
+print(len(palabra_larga))
+
+#Busca la palabra en el texto y arroja True si esta, false si no esta
+phrase = "Every day I wake up then I start to break up, lonely is a man wihtout love, but man, 2 times man"
+buscador = "man" in phrase
+print(buscador)
+
+#Misma funcionalidad pero preguntando de forma negativa
+buscador_negativo = "sex" not in phrase
+print(buscador_negativo)
+
+# Transformación de texto
+mayuscula = phrase.upper()
+minuscula = phrase.lower()
+print(mayuscula)
+print(minuscula)
+
+# Limpieza de espacios
+espacios = "   este es el texto   "
+sin_espacios = espacios.strip()
+print(sin_espacios)
+
+
+
