ac1dfb9e2005c9cf7cc360e22a6e2918f1ae42b0
Sebastian Agudelo <sebastian.agudelo083@gmail.com>
2025-12-22 21:37:56 -0500

    Feat : New code, Booleans, Slicing and isinstance

--- sintaxis.py
+++ sintaxis.py
@@ -78,17 +78,17 @@ import random
 print(random.randrange(1, 10))  # entero aleatorio entre 1 y 9
 
 
-"""
+
 
 father = "padre en ingles"
 mother = 'Madre en ingles'
 son = 'Father and mother "project" '
 daugther = " father's and mother's creation "
 palabra_larga = "esternocleidomastoideo"
-triple_frase = """sexo
+triple_frase = '''sexo
 drogas
 y mucho alchol
-"""
+'''
 print(triple_frase)
 
 #Indica la cantidad de caracteres
@@ -97,11 +97,11 @@ print(len(palabra_larga))
 #Busca la palabra en el texto y arroja True si esta, false si no esta
 phrase = "Every day I wake up then I start to break up, lonely is a man wihtout love, but man, 2 times man"
 buscador = "man" in phrase
-print(buscador)
+print(buscador) #True
 
 #Misma funcionalidad pero preguntando de forma negativa
 buscador_negativo = "sex" not in phrase
-print(buscador_negativo)
+print(buscador_negativo) #True
 
 # Transformación de texto
 mayuscula = phrase.upper()
@@ -116,3 +116,59 @@ print(sin_espacios)
 
 
 
+
+texto = "Este es un texto"
+print(texto[0])   # E
+print(texto[4])   # espacio
+
+#Con slicing toca contar uno de mas, empieza con 0,1,2,3,4 , pero en este caso, anadimos uno mas, 
+#basicamente, lo pone de nuevo de 1,2,3,4,
+print(texto[0:16])
+
+
+print(texto[0:4])   # Este
+# Hasta el final contando manualmente: cuidado con la última letra
+print(texto[0:15])  # falta la 'o' final si 15 apunta a esa posición
+
+print(texto[:7])   # "Este es"
+print(texto[5:])   # desde la "e" de "es" hasta el final
+print(texto[5:-2]) # De la "e" de "es" hasta incluir la "x": fin como -2 para abarcar la X
+
+curso = "Este curso es de JavaScript"
+print(curso.replace("JavaScript", "Python"))
+# "Este curso es de Python"
+
+# Si hubiera múltiples "JavaScript", las reemplaza todas
+
+
+texto = "Este es un texto"
+
+texto_dividido = texto.split(" ")
+print(texto_dividido)  # ['Este', 'es', 'un', 'texto']
+
+texto2 = "Este Texto tiene MAYÚSCULAS y minúsculas"
+buscado = "mayúsculas"
+print(buscado in texto2)  # False
+print(buscado.lower() in texto2.lower())  # True
+
+"""
+
+print(5 > 3)  # True
+print(3 > 5)  # False
+
+print(bool("hola mundo"))           # True -> *string* con contenido
+print(bool(123))                       # True -> número con contenido                       # True
+print(bool(["manzana", "pera"]))   # True -> *list* con elementos
+
+
+#False
+print(bool(""))     # False -> *string* vacío
+print(bool(0))       # False -> cero
+print(bool([]))      # False -> *list* vacía
+print(bool(None))    # False -> *None*
+
+x = 3
+print(isinstance(x, int))   # True -> es entero
+
+x = 0.5
+print(isinstance(x, int))   # False -> ahora es flotante (*float*)
